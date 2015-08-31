4e292ca12a0bf80f44ef942b4968f061c5f09433
unknown <mschlecht@NRC-0256.dlss.com>
2015-08-31 16:14:24 -0500

    Commented out code referencing Mongo DB

--- BMApp/app.js
+++ BMApp/app.js
@@ -9,10 +9,10 @@ var morgan = require('morgan');                         // log requests to the c
 var methodOverride = require('method-override');
  
  // Mongo DB setup
-var MongoClient = require('mongodb').MongoClient;
+/*var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var ObjectId = require('mongodb').ObjectID;
-var url = 'mongodb://localhost:27017/test';
+var url = 'mongodb://localhost:27017/test';*/
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -73,11 +73,11 @@ if (app.get('env') === 'development') {
   app.locals.pretty = true;
 }
 
-var url = 'mongodb://localhost:27017/test';
+/*var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected correctly to server.");
   db.close();
-});
+});*/
 
 module.exports = app;
